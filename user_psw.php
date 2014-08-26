6aa65164eeac12b9fb9ca2ca7878fcbed041cdc2
Davin <yeyongfa0515@gmail.com>
2014-08-26 21:27:18 +0800

    the event was modified to use the onchange method.

--- user_psw.php
+++ user_psw.php
@@ -11,7 +11,7 @@ $rows=mysql_fetch_assoc($rs);
 		
 	    <div id="changeArea">
 	    <span id='pswUserid'><?php echo $rows["userid"] ?></span>
-	    <input name="password" type="password" id="password" onblur='originPsw();' class="password" placeholder="原密码"><p id='pswError'></p>
+	    <input name="password" type="password" id="password" onchange='originPsw();' class="password" placeholder="原密码"><p id='pswError'></p>
 	    <input name="password1" type="password" id="password1" class="password" placeholder="新密码">
 	    <input  name="password2" type="password" id="password2" class="password" onblur='check();' placeholder="确认新密码">
 	    <input id="changePw" class="" onclick='changePsw();' type="submit" name="Submit" value="确定更改">
