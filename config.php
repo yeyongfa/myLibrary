011aadc78deb8504b591240de967b6aa250ee625
Davin <yeyongfa0515@gmail.com>
2014-08-28 10:38:52 +0800

    修改数据库连接数据

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 	error_reporting(0);
 	ob_start();
 	session_start(); //开启缓存
-	$conn=@mysql_connect('localhost','root','');  //配置mysql服务器信息"59.188.182.239","a0308181112","6793481"
+	$conn=@mysql_connect('localhost','root','');  
 	if($conn==null)
 	{
 		echo "很抱歉，数据库打开失败哟";
 		exit; //数据库打开失败，退出
 	}
-	mysql_query("SET NAMES 'utf8'"); //设置数据库编码
-	mysql_select_db("bookinfo", $conn); //a0308181112
+	mysql_query("SET NAMES 'utf8'"); 
+	mysql_select_db("bookinfo", $conn); 
 ?>
